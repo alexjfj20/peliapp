9a18ceb9e6cd025d1841569cb203acc3c72fac7b
alexjfj20 <auditsemseo@gmail.com>
2023-11-05 12:51:30 -0500

    fist accommit

--- src/routes/directors.js
+++ src/routes/directors.js
@@ -1,6 +1,8 @@
 const { getAll } = require('../controllers/directors.controllers');
 const express = require('express');
 
+// se crea la rutas
+
 const directorsRouter = express.Router();
 
 directorsRouter.route('/')
